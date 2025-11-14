e43bc74df111cbbd787cd070544b30186abaaafa
DEV Jefferson <125227918+studentcs502023@users.noreply.github.com>
2025-11-13 21:54:56 -0500

    Update App.vue

--- carrito-compra/src/App.vue
+++ carrito-compra/src/App.vue
@@ -1,113 +1,204 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue'
-import '@fortawesome/fontawesome-free/css/all.min.css'
+import { ref } from 'vue'
+import { computed } from 'vue'
+const mostrar = ref(false)
 
+
+
+const productos = ref([
+  {
+    id: 1,
+    nombre: "Laptop Dell",
+    precio: 899,
+    imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4aCZn0CFEQ_RhFyois7kdXlqnzyetD5JT4Q&s",
+    mostrar: false,
+    cantidad:0,
+    total:0,
+    subtotal:0,
+  },
+  {
+    id: 2,
+    nombre: "Mouse Logitech",
+    precio: 25,
+    imagen: "https://www.alkosto.com/medias/097855183453-001-750Wx750H?context=bWFzdGVyfGltYWdlc3w4MzY0fGltYWdlL3dlYnB8YUdZd0wyaGxPQzh4TkRRM05ERTJNRGs1TmpNNE1pOHdPVGM0TlRVeE9ETTBOVE5mTURBeFh6YzFNRmQ0TnpVd1NBfDA0MTcyMjc3MDFjYTNhMTg0ZmYxZDMwMDVhOWI2ZGMwZjMzYjM1OTE4ZGM4NTRlOWU4ZjRiNGFiMGI3NTQ5NGY",
+    mostrar: false,
+    cantidad:0,
+    total:0,
+    subtotal:0,
+  },
+  {
+    id: 3,
+    nombre: "Teclado mecánico",
+    precio: 89,
+    imagen: "https://media.falabella.com/falabellaCO/128921005_01/w=1500,h=1500,fit=pad",
+    mostrar: false,
+    cantidad:0,
+    total:0,
+    subtotal:0,
+  },
+  {
+    id: 4,
+    nombre: "Webcan HD",
+    precio: 59,
+    imagen: "https://media.falabella.com/falabellaCO/119228400_01/w=800,h=800,fit=pad",
+    mostrar: false,
+    cantidad:0,
+    total:0,
+    subtotal:0,
+  
+  }
+])
+
+function activarProducto(producto) {
+  // activar v-show
+  producto.mostrar = !producto.mostrar
+
+  // si se activó, agregar 1 unidad
+  if (producto.mostrar) {
+    producto.cantidad = 1
+    producto.total = producto.precio
+  } else {
+    // si se oculta, volver a cero
+    producto.cantidad = 0
+    producto.total = 0
+  }
+}
+
+
+function agregarAlCarrito(producto) {
+  producto.cantidad++
+  producto.total = producto.cantidad * producto.precio
+}
+
+function quitarAlCarrito(producto) {
+  if (producto.cantidad > 0) {
+    producto.cantidad--
+    producto.total = producto.cantidad * producto.precio
+  }
+}
+const totalProductos = computed(() =>
+  productos.value.reduce((acumulador, p) => acumulador + p.cantidad, 0)
+)
+const subtotal = computed(()=>
+
+productos.value.reduce((acumulador, p)=> acumulador + p.total, 0))
+
+
+
+
+const valor = computed(() => subtotal.value * 0.16)
+
+const totalapgar = computed(() => valor.value + subtotal.value)
 
 </script>
 
 <template>
   <div>
-<div class="infocar">
-<span><i class="fa-solid fa-cart-shopping"></i></span>
-<h2>carrito de compra</h2>
-</div>
+  <div>
+  
+    <div class="infocar">
+      
+      <h2><q-btn square color="blue" icon="shopping_cart" />carrito de compra</h2>
+    </div>
+<div class="contenedorpro">
+  <div class="destacado">
+    <q-chip v-if="totalapgar>1000" outline color="teal" text-color="white" icon="bookmark">
+      Felicidades has ganado un evió gratis tu compra supera los 1000usd
+    </q-chip>
+  </div>
+  <div class="infopro">
+    <h3>Productos Disponibles</h3>
 
-<div class="infopro">
-<h3>Productos Disponibles</h3>
-<div class="p1">
-<img src="" alt="imag">
-<h4>laptop Dell <br>
-<span>$8.99</span></h4>
-<div class="b1">
-  <!-- Botón principal del carrito -->
-  <button class="btn-cart">
-    <i class="fa-solid fa-cart-plus"></i>
-  </button>
-  <!-- Contenedor con los dos botones + y - -->
-  <div class="mostrar">
-    <button class="btn-mas">+</button>
-    <button class="btn-menos">−</button>
+    <!-- SOLO UN v-for -->
+    <div
+      class="p1"
+      v-for="p in productos"
+      :key="p.id"
+    >
+      <img class="size" :src="p.imagen" alt="imagen" style="width: 9vw; margin: 10px;" >
+
+      <h4 style="font-weight: bold;">
+        {{ p.nombre }}
+        <br>
+        <span style="color: greenyellow;">${{ p.precio }}</span>
+      </h4>
+
+      <div class="b1">
+
+        <!-- ESTE BOTÓN ACTIVA Y AGREGA 1 UNIDAD -->
+        <q-btn 
+          color="secondary"
+          text-color="black"
+          style="border-radius: 20px;"
+          label="Agregar al carrito"
+          @click="activarProducto(p)"
+         v-if="!p.mostrar"
+        />
+
+        <!-- BOTONES + y - -->
+        <div class="divapa" v-show="p.mostrar">
+          <q-btn
+            square
+            color="black"
+            icon="add"
+            @click="agregarAlCarrito(p)"
+          />
+
+          <q-btn
+            square
+            color="black"
+            icon="remove"
+            @click="quitarAlCarrito(p)"
+          />
+        </div>
+
+        <!-- Mostrar cantidades -->
+        <p >Cantidad: {{ p.cantidad }}</p>
+        <p >Total: ${{ p.total }}</p>
+
+      </div>
+    </div>
+
   </div>
 </div>
 
-</div>
-
-<div class="p1">
-<img src="" alt="imag">
-<h4>laptop Dell <br>
-<span>$8.99</span></h4>
-<div class="b1">
-<button></button>
-</div>
-
-</div>
-
-<div class="p1">
-<img src="" alt="imag">
-<h4>laptop Dell <br>
-<span>$8.99</span></h4>
-<div class="b1">
-<button></button>
-</div>
-
-</div>
-
-<div class="p1">
-<img src="" alt="imag">
-<h4>laptop Dell <br>
-<span>$8.99</span></h4>
-<div class="b1">
-<button></button>
-</div>
-
-</div>
-
-<div class="p1">
-<img src="" alt="imag">
-<h4>laptop Dell <br>
-<span>$8.99</span></h4>
-<div class="b1">
-<button></button>
-</div>
-
-</div>
-
+  </div>
 
 <div class="resumen">
 <div class="info">
 
-<span><i class="fa-solid fa-chart-simple"></i></span>
-<h2>Resumen carrito</h2>
+
+<h2><i class="fa-solid fa-chart-simple"></i>Resumen carrito</h2>
 <p>vacio</p>
 </div>
-<div class="ventas">
+<div class="contenedor-ventas">
   <table>
-    <thead>
+    <thead class="ventas">
       <tr>
         <th>Productos</th>
         <th>Valores Items</th>
       </tr>
     </thead>
 
-    <tbody>
+    <tbody class="ventas1">
       <tr>
-        <td>Total productos:</td>
-        <td>vacio </td>
+       <td>Total productos: </td>
+       <td>{{ totalProductos }}</td>
       </tr>
 
       <tr>
         <td>Subtotal:</td>
-        <td>vacio</td>
+        <td>{{ subtotal }}</td>
       </tr>
 
       <tr>
         <td>Impuesto:</td>
-        <td>vacio</td>
+        <td>16%</td>
       </tr>
 
 <tr>
         <td>Total a pagar:</td>
-        <td>vacio</td>
+        <td>{{ totalapgar }}</td>
       </tr>
     </tbody>
   </table>
@@ -117,23 +208,56 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 </div>
 
 </div>
-  </div>
+
   
 </template>
 
 <style >
-.size{
-height: auto;
-width: 10vw;
+
+body{
+background-color:greenyellow ;
 }
+
+.info{
+  display: flex;
+  flex-direction: column;
+}
+
+.infocar h2{
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase
+}
+.infopro h3{
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.contenedorpro{
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background-color:goldenrod ;
+
+}
+
 .p1{
 display: flex;
 flex-direction: row;
-width: 100%;
+width: 80%;
 gap: 20px;
 justify-content: center;
 align-items: center;
+
 }
+
+.size{
+  width: 30vw;
+  height: auto;
+}
+
+
+
 .b1{
 display: flex;
 justify-content: center;
@@ -141,9 +265,21 @@ align-items: center;
 margin-left: 150px;
 
 }
-
+.divapa{
+  gap: 10px;
+}
 .b1 button{
 background-color: bisque;
+}
+
+.p1 img{
+  width: 30px;
+  height: auto;
+}
+
+.ventas1 :nth-child(4) {
+  text-decoration: underline  red;
+  text-decoration-thickness: 6px;
 }
 
 </style>
